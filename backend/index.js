e898103e446948145b30a141cacdd2f1519804f9
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2023-06-28 17:07:19 +0530

    added basic schema and api routes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ connectToDB(process.env.MONGO_URI)
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
