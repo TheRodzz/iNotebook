3b0b367912fa3b830b50a6318a05eb9a4f398568
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2023-06-28 21:53:52 +0530

    added try-catch block for user creation

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config()
 connectToDB(process.env.MONGO_URI)
 const app = express()
-const port = 3000
+const port = 5000
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const {body,validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.json([]);
 })
 
-router.post('/',[
-    body('name','Name must be atleast 3 characters long').isLength({min: 3}),
-    body('userName','Username must be atleast 3 characters long').isLength({min: 3}),
-    body('password','Password must be at least 8 characters long').isLength({min: 8})
-], (req,res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({error: errors.array()});
+// create a user using: POST "/api/auth/createuser". no login required
+router.post('/createuser', [
+    body('name', 'Name must be atleast 3 characters long').isLength({ min: 3 }),
+    body('userName', 'Username must be atleast 3 characters long').isLength({ min: 3 }),
+    body('password', 'Password must be at least 8 characters long').isLength({ min: 8 })
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    // if there are errors, return bad request along with errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
     }
-    
-    User.create({
-        name: req.body.name,
-        userName: req.body.userName,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => res.json({error: 'please enter a unique value for username'}))
+    // check wether a user with same username exists
+    try {
+
+        let user = await User.findOne({ userName: req.body.userName });
+        if (user) {
+            return res.status(400).json({ error: "User with this username already exists" })
+        }
+        user = await User.create({
+            name: req.body.name,
+            userName: req.body.userName,
+            password: req.body.password,
+        })
+        // .then(user => res.json(user))
+        // .catch(err => res.json({error: 'please enter a unique value for username'}))
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("some error occured");
+    }
 })
 module.exports = router
